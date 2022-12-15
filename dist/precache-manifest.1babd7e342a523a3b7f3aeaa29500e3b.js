9b1864311fd534cec2cf78552098921a1ed8b394
masthoni <119782845+masthoni@users.noreply.github.com>
2022-12-15 18:13:05 +0700

    Initial

---
+++ dist/precache-manifest.1babd7e342a523a3b7f3aeaa29500e3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0695585a2961261c64f82968578477f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf146408df039269aa29",
+    "url": "/static/css/2.f5a5b9ef.chunk.css"
+  },
+  {
+    "revision": "878c00f900d87f9bbb21",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf146408df039269aa29",
+    "url": "/static/js/2.604d0dfb.chunk.js"
+  },
+  {
+    "revision": "cb938dccd0a77ebcaf14af8f922c05d3",
+    "url": "/static/js/2.604d0dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9bc2109c1f63432566",
+    "url": "/static/js/3.a01ff7ce.chunk.js"
+  },
+  {
+    "revision": "878c00f900d87f9bbb21",
+    "url": "/static/js/main.74e7cd86.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.74e7cd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8705ca4710e8730fb5f2",
+    "url": "/static/js/runtime-main.1dac7fe8.js"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/static/media/arrow_right.d9ccc77c.svg"
+  }
+]);
